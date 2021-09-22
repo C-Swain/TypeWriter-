8b39ff7ccfa410a48f84f5705cdcd68d3e25e452
C-Swain <catherinemexicoswain@gmail.com>
2021-09-22 14:27:32 +0000

    fixxed bug

--- typewriter1.js
+++ typewriter1.js
@@ -1,14 +1,16 @@
 
 
-const sentence = "Busty the cat says hi!\n";
+const sentence = "Busty the cat says hi!";
+const newSentence =  `${sentence} \n`
 count = 0;
 const typewriter = () => {
-for (const char of sentence) {
+for (const char of newSentence) {
 	let timer = Number(count);
 	count += 1;
 	setTimeout(() => process.stdout.write(char), 50 * timer )
 	
 }
+process.stdout.write('\n')
 };
 
 
